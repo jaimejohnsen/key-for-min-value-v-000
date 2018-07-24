449ffed5d67b260eb98ced6cb4358be406932034
Jaime Johnsen <drivetraindiva@gmail.com>
2018-07-24 01:08:19 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -9,6 +9,7 @@ def key_for_min_value(name_hash)
 #  if the value is lower save it to compare to the rest of the values
 #   if it's lowest value save it's key name.
 lowest_val = nil
+lowest_key = nil
     name_hash.each do |name, int|
         if int < lowest_val
         lowest_val = int
